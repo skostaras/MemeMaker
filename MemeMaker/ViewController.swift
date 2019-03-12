431cb203c1d83bf492e70c6f2dce043eb7e6984e
Stefanos Kostaras <stefanoskostaras@hotmail.com>
2019-03-12 21:02:33 +0200

    Initial Commit

---
+++ MemeMaker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMaker
+//
+//  Created by Stefanos Kostaras on 12/03/2019.
+//  Copyright © 2019 StefanosK27. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
